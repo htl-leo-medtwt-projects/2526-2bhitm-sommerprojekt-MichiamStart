be4525ce2f3871998cb8015223db0d62ac8efbcb
Michael Zeinhofer <z.michi2009@gmail.com>
2026-04-10 12:50:02 +0200

    Fixed game not loading added link to game page

--- project/Vengence/main/game/script/phaserMain.js
+++ project/Vengence/main/game/script/phaserMain.js
@@ -1,25 +1,58 @@
 const config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  width: window.innerWidth,
+  height: window.innerHeight,
+  pixelArt: true,
   physics: { default: "arcade" },
-  scene: { preload, create, update }
+  scene: { preload, create, update },
+  scale: {
+    mode: Phaser.Scale.RESIZE,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
 };
 
 const game = new Phaser.Game(config);
 
 function preload() {
-  this.load.image("player", "../assets/sprites/placeholderPlayer.png");
+  this.load.image("player", "assets/sprites/placeholderPlayer.gif");
+  this.load.image("DungeonTiles", "assets/worldData/dungeon/Dungeon_Tiles.png");
+  this.load.image("DungeonTile", "assets/worldData/dungeon/Dungeon_Tile.png");
+  this.load.tilemapTiledJSON("map", "assets/worldData/dungeon/dungeon.tmj");
 }
 
 function create() {
+  const map = this.make.tilemap({ key: "map" });
+
+  const tilesets = [
+    map.addTilesetImage("Dungeon_Tiles", "DungeonTiles"),
+    map.addTilesetImage("Dungeon_Tile", "DungeonTile")
+  ];
+
+  const ground = map.createLayer("Boden", tilesets, 0, 0);
+  const deco = map.createLayer("Deko", tilesets, 0, 0);
+  const walls = map.createLayer("Wände", tilesets, 0, 0);
+
+  walls.setCollisionByExclusion([-1]);
+
   this.player = this.physics.add.sprite(400, 300, "player");
+  this.player.setScale(0.25);
+  this.player.setScale(0.25).refreshBody();
+  this.physics.add.collider(this.player, walls);
+  this.player.setDrag(1000);
+
+  this.cameras.main.startFollow(this.player);
+  this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+  this.cameras.main.setZoom(2);
+
+  this.cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update() {
-  const speed = 2;
-  if (this.input.keyboard.isDown("LEFT")) this.player.x -= speed;
-  if (this.input.keyboard.isDown("RIGHT")) this.player.x += speed;
-  if (this.input.keyboard.isDown("UP")) this.player.y -= speed;
-  if (this.input.keyboard.isDown("DOWN")) this.player.y += speed;
+  const speed = 80;
+
+  if (this.cursors.left.isDown) this.player.setVelocityX(-speed);
+  if (this.cursors.right.isDown) this.player.setVelocityX(speed);
+  if (this.cursors.up.isDown) this.player.setVelocityY(-speed);
+  if (this.cursors.down.isDown) this.player.setVelocityY(speed);
+
 }
